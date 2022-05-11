665ac9231fc77eb4afbb1e6b9cd36f5b55950c9d
vmercel <vmercel@gmail.com>
2022-05-11 11:23:24 +0100

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,159 @@
+const logotext = "MERCEL VUBANGSI";
+const meta = {
+    title: "Mercel Vubangsi",
+    description: "I’m Mercel Vubangsi, Computational Materials Scientist, Fullstack developper and AI engineering student. I am presently Post-doc fellow and at the International Center for Research in AI and IOT, Near East University, Turkish Republic of Northern Cyprus",
+};
+
+const introdata = {
+    title: "I’m Mercel Vubangsi",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites AND write scientific research codes",
+        third: "I develop mobile apps",
+    },
+    description: "I amd a teacher of math, physics and computer science. Computational science research and fullstack development are my passions. New techs are what I am fond of",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "3+ years experienced PHP/React/Python Developer with hands-on experience in identifying web-based user interactions along with designing & implementing highly-responsive user interface components by deploying PHP/React concepts. Proficient in translating designs & wireframes into high-quality code, and writing application interface code via PHP, JavaScript and ReactJS workflows. Adept at designing and implementing modern algorithms for computational science. ",
+};
+const worktimeline = [{
+        jobtitle: "Programmer, UI component Designer",
+        where: "AI and IoT lab, Near East University",
+        date: "2022",
+    },
+    {
+        jobtitle: "Assistant Lecturer, Webmaster",
+        where: "JImpact Polytechnic Cameroon",
+        date: "2020",
+    },
+    {
+        jobtitle: "Lecturer, Cyber Security Analyst",
+        where: "University of Bamenda Cameroon",
+        date: "2019",
+    },
+    {
+        jobtitle: "Teacher of Math, Computer Science and Physics",
+        where: "Government Bilingual High School Yom III bandjoun",
+        date: "2010",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "PHP",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "MongoDB/MySQL",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "My expertise is available for: responsive visual interface design, layouts and grid systems, visual hierarchy, typography, color theory, animation, accessibility, and A/B testing.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "I specialise in developing custom-built, secure mobile and web apps for businesses.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "providing high quality website solutions that create better results.I will develop a solution for your business that boosts your online visibility and helps you grow.If you’ re serious about your business, and ready to invest in a smarter online strategy and more professional website design, get in touch today.",
+    },
+    {
+        title: "Computational Science Research",
+        description: "I specialise in writting python and bash scripts to automate processes in scientific computing and materials science research. This involves codes for simulating electronic, optical and transport properties of semiconductor nanostructures.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Velena beauty salon. Ecommerce website for a beauty palour. Fully responsive, built on the LAMP stack.",
+        link: "https://velenasalon.com",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://ubastudent.online",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Conference Managment system with integrated dapp in the ethereum blockchain.",
+        link: "https://www.neu-conf.000webhostapp.com",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "Quiz Management website. Easy administer a quiz by uploading questions and class list.",
+        link: "http://vmquiz.42web.io",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "Find your urrent GPS Location with a single click.",
+        link: "http://react-gps.vercel.app",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "Tick-Tack-Toe game with react.",
+        link: "https://react-game.vercel.app",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "Track your expenses. A react app to manage expenses.",
+        link: "https://react-expense-manager.vercel.app",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "My portfolio developed with react.",
+        link: "https://mercel-portfolio.vercel.app",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vubangsi.mercel@uniba.cm",
+    YOUR_FONE: "(533)829-4913",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/vmercel",
+    facebook: "https://facebook.com/vmercel",
+    linkedin: "https://linkedin.com/vmercel",
+    twitter: "https://twitter.com/vmercel",
+    youtube: "https://youtube.com/vmercel",
+    twitch: "https://twitch.com/vmercel",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
